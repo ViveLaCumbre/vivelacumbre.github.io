ee85337e621116a5dea4f55736a63d3c7db17656
Erickvergara97 <81184269+Erickvergara97@users.noreply.github.com>
2021-10-27 17:44:10 -0500

    modal corrections

--- js/programs/modal.js
+++ js/programs/modal.js
@@ -1,14 +1,23 @@
 let cerrar = document.querySelectorAll(".close")[0];
-let abrir = document.querySelectorAll(".cta")[0];
+let abrirU = document.querySelector(".cta-u");
+let abrirV = document.querySelector(".cta-v");
 let modal = document.querySelectorAll(".modal")[0];
 let modalC = document.querySelectorAll(".modal-container")[0];
 
-abrir.addEventListener("click", function(e){
+abrirU.addEventListener("click", function(e){
     e.preventDefault();
     modalC.style.opacity="1";
     modalC.style.visibility="visible";
     modal.classList.toggle("modal-close");
 })
+
+abrirV.addEventListener("click", function(e){
+    e.preventDefault();
+    modalC.style.opacity="1";
+    modalC.style.visibility="visible";
+    modal.classList.toggle("modal-close");
+})
+
 
 cerrar.addEventListener("click", function(){
     modal.classList.toggle("modal-close");
